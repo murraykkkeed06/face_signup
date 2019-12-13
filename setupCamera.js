77fc7359eebb790434a7eb4990b458f9c1e4bf09
= <=>
2019-12-13 10:36:59 +0800

    sucess of fail display

--- setupCamera.js
+++ setupCamera.js
@@ -51,10 +51,12 @@ window.onload = function(){
 
 }
 
+
+
 async function myFunction(){
 
   
-    document.getElementById('load').style.display="block";
+    document.getElementById('load1').style.display="block";
     
     await faceapi.loadSsdMobilenetv1Model('/models')
     await faceapi.loadFaceLandmarkModel('/models')
@@ -98,7 +100,7 @@ async function myFunction(){
         faceMatcher = new faceapi.FaceMatcher(labeledDescriptors)
         
         */
-        document.getElementById('load').style.display="none";
+        document.getElementById('load1').style.display="none";
         alert("Sign up sucess~");
         //console.log(singleResulto1.descriptor)
         //module.exports = singleResulto1.descriptor;
@@ -119,3 +121,159 @@ async function myFunction(){
 }
 
 
+
+function set_webcam(){
+            
+    //document.getElementById("my_camera").style.display="none";
+    Webcam.set({
+    width: 320,
+    height: 240,
+    image_format: 'jpeg',
+    jpeg_quality: 90
+    });
+    Webcam.attach( '#my_camera' );
+
+}
+function reset_webcam(){
+document.getElementById("my_camera").style.display="block";
+document.getElementById("take_btn").style.display="block";
+document.getElementById("recognize_btn").style.display="none";
+//set_webcam();
+document.getElementById("match").src="";
+Webcam.reset();
+
+}
+
+
+function take_snapshot() {
+document.getElementById("my_camera").style.display="none";
+
+document.getElementById("take_btn").style.display="none";
+
+document.getElementById("recognize_btn").style.display="block";
+
+// take snapshot and get image data
+Webcam.snap( function(data_uri) {
+ // display results in page
+ document.getElementById('results').innerHTML = 
+ '<img class="w3-padding w3-card-4" id="match" src="'+data_uri+'"/>';
+ } );
+
+ //console.log(document.getElementById('result').innerHTML);
+ //document.getElementById("my_camera").style.display="none";
+// document.getElementById('my_camera').style.display="block";
+ //reset_webcam();
+ Webcam.reset();
+ 
+}
+            
+            async function recognize(){
+                
+                document.getElementById('load2').style.display="block";
+
+                await faceapi.loadSsdMobilenetv1Model('/models')
+                await faceapi.loadFaceLandmarkModel('/models')
+                await faceapi.loadFaceRecognitionModel('/models')
+               
+
+                match = document.getElementById("match");
+                
+                
+                
+                const singleResultmatch = await faceapi
+                    .detectSingleFace(match)
+                    .withFaceLandmarks()
+                    .withFaceDescriptor()
+                
+                    
+                    //alert("Error Sign In");
+                    //break;
+                
+                
+            
+                document.getElementById('load2').style.display="none";
+                /*
+                $.get("/face_match", {
+                    match: singleResultmatch.descriptor
+                });
+                */
+               $.post("/face_match").then(function(response){
+
+                var responseLength = response.length;
+
+                var arr1 = []
+                var arr2 = []
+                var arr3 = []
+
+                for(var checknum=0 ; checknum<responseLength; checknum++){
+                    for(var i=0; i<128; i++){
+                        arr1.push(response[checknum].face1[i].num);
+                        arr2.push(response[checknum].face2[i].num);
+                        arr3.push(response[checknum].face3[i].num);
+                    } 
+
+                    const labeledDescriptors = [
+                    new faceapi.LabeledFaceDescriptors(
+                        response[checknum].name,
+                        [new Float32Array(arr1), new Float32Array(arr2), new Float32Array(arr3)]
+                    )
+                    ]
+                    arr1 = [];
+                    arr2 = [];
+                    arr3 = [];
+                    faceMatcher = new faceapi.FaceMatcher(labeledDescriptors);
+                    
+                    try{
+                        bestMatch = faceMatcher.findBestMatch(singleResultmatch.descriptor);
+                        var n = bestMatch.toString().search("unknown");
+                        if (n<0){
+                            //document.getElementById("final").innerHTML="Welcome back " + bestMatch.toString();
+                        
+
+                            window.location = '/face_final?name='+bestMatch.toString();
+                        
+
+                            break;
+                        }
+                        
+                    }catch{
+                        console.log("error fetchin face descriptor");
+                        
+                    }
+                    if(checknum == responseLength-1)
+                            //document.getElementById("final").innerHTML="Cannot recognize";
+                            alert("Error Sign In!")
+                            window.location = './login_form.html';
+                    
+                    }
+
+                
+
+               
+            
+                    });
+                //alert("finish recognizing...")
+
+                
+
+
+                
+                
+            //    console.log(bestMatch.toString())
+                
+            }
+            
+            /*
+            const labeledDescriptors = [
+            new faceapi.LabeledFaceDescriptors(
+                username,
+                [singleResulto1.descriptor, singleResulto2.descriptor, singleResulto3.descriptor]
+            )
+            ]
+
+            faceMatcher = new faceapi.FaceMatcher(labeledDescriptors)
+            
+            */
+            
+            
+            
